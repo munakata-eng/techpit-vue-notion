b08c051a02cba9af1e1ec89622a55f61ac98b3f1
Kiyotaka Munakata <meganeinu.xyz@gmail.com>
2021-11-06 06:02:22 +0900

    [2-8] Sassの実装

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="main-page">
-    メインページです
+    <div class="left-menu">
+      左メニュー
+    </div>
+    <div class="right-view">
+      右ビュー
+    </div>
   </div>
 </template>
 
@@ -9,8 +14,17 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .main-page {
-  padding: 10px;
+  display: flex;
+  height: calc(100vh - 60px);
+  .left-menu {
+    width: 350px;
+    background-color: #f7f6f3;
+  }
+  .right-view {
+    flex-grow: 1;
+    padding: 10px;
+  }
 }
 </style>
