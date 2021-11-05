d15ba1715db440141863ec7be91d73812b06bd71
Kiyotaka Munakata <meganeinu.xyz@gmail.com>
2021-11-06 05:45:47 +0900

    [2-5] Font Awesomeの実装

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -14,13 +14,13 @@
       <ul class="navbar-nav">
         <!-- ノートリストページへのリンク -->
         <li class="nav-item">
-          <a class="nav-link" href="#">ノートリスト</a>
+          <a class="nav-link" href="#"><i class="fas fa-file-alt"></i> ノートリスト</a>
         </li>
 
         <!-- 学習ページへのリンク -->
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" data-toggle="dropdown">
-            学習用ページ
+            <i class="fas fa-user-graduate"></i> 学習用ページ
           </a>
           <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="#">学習1</a></li>
