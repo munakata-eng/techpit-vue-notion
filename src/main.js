a472e5dd5d9bd96fa485a329b8c3674b2dddabef
Kiyotaka Munakata <meganeinu.xyz@gmail.com>
2021-11-06 08:43:05 +0900

    [3-2] VueRouterのインストール

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// VueRouterを使用
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes : [
+  ]
+})
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
