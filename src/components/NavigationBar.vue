8573009f141ab4126dc838e8e83d829abde722fa
Kiyotaka Munakata <meganeinu.xyz@gmail.com>
2021-11-06 05:35:26 +0900

    [2-3] Bootstrapの実装

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,7 +1,36 @@
 <template>
-  <div class="navigation-bar">
-    ナビゲーションバーです
-  </div>
+  <!-- ナビゲーションバーをダークテーマで作成 -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <!-- タイトル -->
+    <a class="navbar-brand" href="#">TechpitNotion</a>
+
+    <!-- 画面サイズに応じて折りたたむ設定  -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- メニュー全体 -->
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav">
+        <!-- ノートリストページへのリンク -->
+        <li class="nav-item">
+          <a class="nav-link" href="#">ノートリスト</a>
+        </li>
+
+        <!-- 学習ページへのリンク -->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-toggle="dropdown">
+            学習用ページ
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">学習1</a></li>
+            <li><a class="dropdown-item" href="#">学習2</a></li>
+            <li><a class="dropdown-item" href="#">学習3</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -12,7 +41,5 @@ export default {
 <style scoped>
 .navigation-bar {
   height: 60px;
-  color: white;
-  background-color: #6c757d;
 }
 </style>
