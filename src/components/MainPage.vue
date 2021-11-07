79390abe410e4f9cf5105a9c7879735b05f82713
Kiyotaka Munakata <meganeinu.xyz@gmail.com>
2021-11-07 20:13:36 +0900

    [4-4] ノートの見た目を変えてみよう

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -33,6 +33,7 @@ export default {
       this.noteList.push({
         id : new Date().getTime().toString(16),
         name : `新規ノート`,
+        mouseover : false,
       })
     },
   },
--- src/components/parts/NoteItem.vue
+++ src/components/parts/NoteItem.vue
@@ -1,9 +1,29 @@
 <template>
-  <div class="note">
+  <div class="note"
+    @mouseover="onMouseOver"
+    @mouseleave="onMouseLeave"
+    v-bind:class="{mouseover: note.mouseover}"
+  >
     <div class="note-icon">
       <i class="fas fa-file-alt"></i>
     </div>
     <div class="note-name">{{note.name}}</div>
+
+    <div v-show="note.mouseover" class="buttons">
+      <div class="button-icon">
+        <i class="fas fa-sitemap"></i>
+      </div>
+      <div class="button-icon">
+        <i class="fas fa-plus-circle"></i>
+      </div>
+      <div class="button-icon">
+        <i class="fas fa-edit"></i>
+      </div>
+      <div class="button-icon">
+        <i class="fas fa-trash"></i>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -13,6 +33,14 @@ export default {
   props: [
     'note',
   ],
+  methods: {
+    onMouseOver : function() {
+      this.note.mouseover = true;
+    },
+    onMouseLeave : function() {
+      this.note.mouseover = false;
+    },
+  },
 }
 </script>
 
@@ -23,12 +51,25 @@ export default {
   align-items: center;
   padding: 5px;
   color: rgba(25, 23, 17, 0.6);
+  &.mouseover {
+    background-color: rgb(232, 231, 228);
+    cursor: pointer;
+  }
   .note-icon {
     margin-left: 10px;
   }
   .note-name {
     width: 100%;
     padding: 3px 10px;
+  }
+  .buttons {
+    display: flex;
+    flex-direction: row;
+    .button-icon {
+      padding: 3px;
+      margin-left: 3px;
+      border-radius: 5px;
+    }
   }
 }
 </style>
